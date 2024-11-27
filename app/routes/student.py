9d82ffe15fdec0f1288c01c32ba84369281ed554
Parth Shrotriya <paarthshrotriya12@gmail.com>
2024-11-28 00:27:14 +0530

    wrong things happened

--- app/routes/student.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-student_bp = Blueprint('student', __name__)
-
-@student_bp.route('/')
-def student_dashboard():
-    return render_template('student_dashboard.html')
